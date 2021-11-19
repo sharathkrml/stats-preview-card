1e1c955f8ccf2867c2d8898578757dac64f8722f
sharath V <sharathkrml@gmail.com>
2021-11-19 11:15:36 +0530

    added image overlay and card sizing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import headerImg from './assets/images/image-header-desktop.jpg'
 function App() {
   return (
     <div className="App">
+      <div className="card">
+        <div className="card-body">
+          <div className="main-heading">
+            Get insights that help your business grow.
+          </div>
+          <div className="main-paragraph">
+            Discover the benefits of data analytics and make better decisions
+            regarding revenue, customer experience, and overall efficiency.
+          </div>
+          <div className="stats">
+            <div className="stats-body">
+              <div className="stats-main">10k+</div>
+              <div className="stats-desc">companies</div>
+            </div>
+            <div className="stats-body">
+              <div className="stats-main">314</div>
+              <div className="stats-desc">templates</div>
+            </div>
+            <div className="stats-body">
+              <div className="stats-main">12m+</div>
+              <div className="stats-desc">queries</div>
+            </div>
+          </div>
+        </div>
+        <div className="card-img">
+          <img src={headerImg} alt="headerImage" />
+        </div>
+      </div>
     </div>
   );
 }
