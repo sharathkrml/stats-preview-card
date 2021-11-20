80ba0e5f0f5091fea2a1c73b83c88ac13731bb65
sharath V <sharathkrml@gmail.com>
2021-11-20 11:59:43 +0530

    added moile view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
-import headerImg from './assets/images/image-header-desktop.jpg'
+import headerImg from "./assets/images/image-header-mobile.jpg";
 function App() {
   return (
     <div className="App">
       <div className="card">
+        <div className="card-img">
+          <img src={headerImg} alt="headerImage" />
+        </div>
         <div className="card-body">
           <div className="main-heading">
             Get <span>insights</span> that help your business grow.
@@ -15,20 +18,17 @@ function App() {
           <div className="stats">
             <div className="stats-body">
               <div className="stats-main">10k+</div>
-              <div className="stats-desc">companies</div>
+              <div className="stats-desc">COMPANIES</div>
             </div>
             <div className="stats-body">
               <div className="stats-main">314</div>
-              <div className="stats-desc">templates</div>
+              <div className="stats-desc">TEMPLATES</div>
             </div>
             <div className="stats-body">
               <div className="stats-main">12M+</div>
-              <div className="stats-desc">queries</div>
+              <div className="stats-desc">QUERIES</div>
             </div>
           </div>
-        </div>
-        <div className="card-img">
-          <img src={headerImg} alt="headerImage" />
         </div>
       </div>
     </div>
